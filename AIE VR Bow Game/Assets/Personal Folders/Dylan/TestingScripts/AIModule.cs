e835fb027e562fd7c052f4f25149914717ec42ff
Lomie02 <dylansmith1844@gmail.com>
2022-06-17 14:48:08 +1000

    Added events & set up functions for future

--- AIE VR Bow Game/Assets/Personal Folders/Dylan/TestingScripts/AIModule.cs
+++ AIE VR Bow Game/Assets/Personal Folders/Dylan/TestingScripts/AIModule.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Events;
 
 [System.Obsolete]
 public class AIModule : MonoBehaviour
@@ -16,8 +17,9 @@ public class AIModule : MonoBehaviour
     {
         SHOOT = 0,
         MOVETOPLAYER,
-        MOVEAWAY,
+        RETREAT,
         STUN,
+        DEATH,
     }
 
     //==============================================================
@@ -26,18 +28,31 @@ public class AIModule : MonoBehaviour
     PlayerAssignSettings m_PlayerAssignment = PlayerAssignSettings.MANUALLY;
 
     [SerializeField] GameObject m_PlayerTarget = null; // The player in the scene.
-    NavMeshAgent m_EnemyAgent = null;
+
+    [Header("AI Events")]
+    [SerializeField] UnityEvent m_OnEnterStun;
+    [SerializeField] UnityEvent m_OnExitStun;
+
+    [Space()]
+
+    [SerializeField] UnityEvent m_OnMove;
+    [SerializeField] UnityEvent m_OnDeath;
+
+    [SerializeField, Range(10, 50)] float m_StoppingDistance = 10;
+    [SerializeField] float m_FleeDistance = 5;
 
     //=====================================================
     [Space()]
-    [SerializeField, Range(0, 10)] float m_StunnedTimer = 5;
+    [SerializeField, Range(0, 10)] float m_StunTime = 5;
 
-    public float m_StunTimer = 0;
+    float m_StunTimer = 0;
     bool m_IsStuned = false;
-    [SerializeField] float m_Range;
 
     EnemyStates m_EnemyStates;
     Rigidbody m_Rigidbody = null;
+    NavMeshAgent m_EnemyAgent = null;
+
+    bool m_IsAlive = true;
 
     //==============================================================
     void Start()
@@ -45,14 +60,15 @@ public class AIModule : MonoBehaviour
         m_EnemyAgent = GetComponent<NavMeshAgent>();
         m_Rigidbody = GetComponent<Rigidbody>();
 
-        m_StunTimer = m_StunnedTimer;
+        m_StunTimer = m_StunTime;
 
         if (m_PlayerAssignment == PlayerAssignSettings.TAG)
         {
             GameObject Temp = GameObject.FindGameObjectWithTag("Player");
             m_PlayerTarget = Temp;
         }
-        Stun();
+
+        m_FleeDistance = m_StoppingDistance - 5;
     }
 
     void Update()
@@ -63,23 +79,35 @@ public class AIModule : MonoBehaviour
                 m_EnemyAgent.SetDestination(m_PlayerTarget.transform.position);
                 break;
 
-            case EnemyStates.STUN: //Stop attacking
+            case EnemyStates.STUN:
+
                 m_StunTimer -= Time.deltaTime;
                 if (m_StunTimer <= 0)
                 {
                     m_IsStuned = false;
-                    m_StunTimer = m_StunnedTimer;
-                    CheckDistanceToPlayer();
+                    m_StunTimer = m_StunTime;
+
+                    if (m_OnExitStun != null)
+                    {
+                        m_OnExitStun.Invoke();
+                    }
                 }
 
                 break;
 
-            case EnemyStates.SHOOT: // shoot at the player
+            case EnemyStates.SHOOT:
 
+                //TODO fire weapons at player
                 break;
 
-            case EnemyStates.MOVEAWAY:
-                m_EnemyAgent.SetDestination(-m_PlayerTarget.transform.position);
+            case EnemyStates.RETREAT:
+
+                //TODO flee to a position 
+                break;
+
+            case EnemyStates.DEATH:
+
+                //TODO do stuff when agent dies
                 break;
         }
         CheckDistanceToPlayer();
@@ -87,32 +115,86 @@ public class AIModule : MonoBehaviour
 
     void CheckDistanceToPlayer()
     {
-        if (m_IsStuned) return;
+        if (m_IsStuned || !m_IsAlive)
+        {
+            return;
+        }
 
         float DistanceBetween = Vector3.Distance(m_PlayerTarget.transform.position, transform.position);
 
-        if (DistanceBetween > m_Range)
+        if (DistanceBetween > m_StoppingDistance)
         {
-            m_EnemyStates = EnemyStates.MOVETOPLAYER;
+            Move();
         }
-        else
+        else if(DistanceBetween < m_StoppingDistance && DistanceBetween > m_FleeDistance)
         {
             if (!m_EnemyAgent.isStopped)
             {
+                Debug.Log("Too Close!");
+
                 m_EnemyAgent.Stop();
                 m_EnemyAgent.transform.LookAt(m_PlayerTarget.transform.position, Vector3.up);
             }
         }
+        else if (DistanceBetween < m_FleeDistance)
+        {
+            Flee();
+        }
     }
 
+    //==================================================
+    // Stun the Enemy for a short period 
     public void Stun()
     {
-        if (!m_IsStuned)
+        if (!m_IsStuned && m_IsAlive)
         {
             m_IsStuned = true;
+            if (m_OnEnterStun != null)
+            {
+                m_OnEnterStun.Invoke();
+            }
+
             m_EnemyAgent.Stop();
-            m_StunTimer = m_StunnedTimer;
+            m_StunTimer = m_StunTime;
             m_EnemyStates = EnemyStates.STUN;
         }
+    }
+
+    private void Move()
+    {
+        if (!m_IsStuned && m_EnemyStates != EnemyStates.MOVETOPLAYER && m_IsAlive)
+        {
+            if (m_OnMove != null)
+            {
+                m_OnMove.Invoke();
+            }
+
+            m_EnemyStates = EnemyStates.MOVETOPLAYER;
+        }
+    }
+
+    private void Flee()
+    {
+        if (m_EnemyStates != EnemyStates.RETREAT)
+        {
+            //  TODO change state to flee
+        }
+    }
+
+    private void Shoot()
+    {
+        //TODO change state to shoot
+    }
+
+    public void Kill()
+    {
+        m_OnDeath.Invoke();
+        m_IsAlive = false;
+        m_EnemyStates = EnemyStates.DEATH;
+    }
+
+    public bool GetAliveState()
+    {
+        return m_IsAlive;
     }
 }
